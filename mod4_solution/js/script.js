8a13fa03579b327719ba903d1f3a4dd1e8ed428f
SagarSRathod117 <130021873+SagarSRathod117@users.noreply.github.com>
2023-04-10 15:34:48 +0530

    Update script.js

--- mod4_solution/js/script.js
+++ mod4_solution/js/script.js
@@ -6,10 +6,6 @@
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
-  (names[i].charAt(0).toLowerCase() == 'j') ? console.log("GoodBye "+names) : console.log("Hello "+names);
+  (names[i].charAt(0).toLowerCase() == 'j') ? console.log("GoodBye "+names[i]) : console.log("Hello "+names[i]);
 
 }
-
-
-
-
